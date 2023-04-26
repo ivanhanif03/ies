06d1b2f59643631b38d3e63c6e74ad45d7a0fd9b
Ivan hanif <ivanhanif03@gmail.com>
2023-04-26 11:16:27 +0700

    .

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -51,6 +51,27 @@
     });
 </script>
 
+<script type="text/javascript">
+    document.onkeydown = function(e) {
+        if (event.keyCode == 123) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+            return false;
+        }
+
+    }
+</script>
+
 <!-- Page Specific JS File -->
 <?= $this->renderSection('script'); ?>
 </body>
